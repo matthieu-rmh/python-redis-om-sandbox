8be75da65dace8ccf6b4f6924f50df3a0d86e984
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-10-27 00:44:38 +0300

    feat: removed standard redis instance and replace it with redis-stack-server as std one doesn't support redis json out of the box

--- run.py
+++ run.py
@@ -1,23 +1,21 @@
 from redis_om import HashModel, JsonModel, get_redis_connection
 
-redis_conn = get_redis_connection(url="redis://localhost:6379")
 
 class SampleData(JsonModel):
     title: str
     code: int
-    class Meta:
-        database=redis_conn
 
 
 class User(HashModel):
     name: str
     mail: str
 
+
 def main():
-    francois = User(name="François", mail="françois@mail.com")
-    # data_sample = SampleData(title="document 1", code=89)
-    # data_sample.save()
-    print(francois)
+    # francois = User(name="François", mail="françois@mail.com")
+    data_sample = SampleData(title="document 1", code=89)
+    data_sample.save()
+    print(data_sample)
 
 if __name__ == "__main__":
     main()
